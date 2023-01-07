04733a46ff1df60ab9ce81805faa09904bac9082
Jonny Noone <jonny.f.noone@gmail.com>
2023-01-07 16:22:47 +0000

    facts loaded from Supabase

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
-const initialFacts = [
-    {
-        id: 1,
-        text: "React is being developed by Meta (formerly facebook)",
-        source: "https://opensource.fb.com/",
-        category: "technology",
-        votesInteresting: 24,
-        votesMindblowing: 9,
-        votesFalse: 4,
-        createdIn: 2021,
-    },
-    {
-        id: 2,
-        text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-        source:
-        "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-        category: "society",
-        votesInteresting: 11,
-        votesMindblowing: 2,
-        votesFalse: 0,
-        createdIn: 2019,
-    },
-    {
-        id: 3,
-        text: "Lisbon is the capital of Portugal",
-        source: "https://en.wikipedia.org/wiki/Lisbon",
-        category: "society",
-        votesInteresting: 8,
-        votesMindblowing: 3,
-        votesFalse: 1,
-        createdIn: 2015,
-    },
-];
-
 // Selecting DOM Elements
 const shareBtn = document.querySelector('.btn-open');
 const form = document.querySelector('.fact-form');
 const factsList = document.querySelector('.facts-list');
 
-// Create DOM Elements
+// Clear static DOM Elements
 factsList.innerHTML = '';
-// DO the FACTS
-createFactsList(initialFacts);
+
+// Load data from Supabase
+async function loadFacts() {
+    const res = await fetch('https://qjfjaexrrhqrlhpqsaeh.supabase.co/rest/v1/facts', {
+        headers: {
+            apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZmphZXhycmhxcmxocHFzYWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMwMTA4OTksImV4cCI6MTk4ODU4Njg5OX0.DEM3VnXr-G4GLyg-wd2pveZyRFBcKzj-RhjCV0OvpvI',
+            authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqZmphZXhycmhxcmxocHFzYWVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMwMTA4OTksImV4cCI6MTk4ODU4Njg5OX0.DEM3VnXr-G4GLyg-wd2pveZyRFBcKzj-RhjCV0OvpvI'
+        }
+    });
+    
+    const data = await res.json();
+    createFactsList(data);
+}
+loadFacts();
 
 function createFactsList(dataArr) {
     // Generate HTML from data
