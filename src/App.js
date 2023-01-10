1640b1f1e310883fbab61c548cbc542362fc0fa4
Jonny Noone <jonny.f.noone@gmail.com>
2023-01-10 18:47:48 +0000

    final css tweaks

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState('');
-  const [source, setSource] = useState('http://example.com');
+  const [source, setSource] = useState('');
   const [category, setCategory] = useState('');
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -193,6 +193,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed = fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(voteType) {
     setIsUpdating(true);
@@ -216,6 +217,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⚠️ DISPUTED] </span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank" rel="noreferrer">(Source)</a>
       </p>
